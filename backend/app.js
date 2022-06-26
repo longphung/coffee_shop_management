33adde7214404f5333f21662a722c79eee212a2b
Phung Nguyen <longphung.nn@gmail.com>
2022-06-26 12:48:41 +0700

    ✨ feat: handle routes

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,30 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+handleCoffeeRoute = (res) => {
+  const data = [{
+    "name": "Cà phê đá", "price": 10000, "currency": "VND"
+  }, {
+    "name": "Cà phê sữa", "price": 10000, "currency": "VND"
+  }]
+}
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  switch (req.url) {
+    case "/":
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello World');
+      break;
+    case "/coffee":
+      handleCoffeeRoute(res);
+      break;
+    default:
+      res.statusCode = 404;
+      res.end("Route not found");
+      break;
+  }
 });
 
 server.listen(port, hostname, () => {
