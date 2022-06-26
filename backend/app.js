6fd3255186de84326d79f3c269d753185e4d98a8
Loc Phan <pptloc28@Gmail.com>
2022-06-26 13:49:26 +0700

    handle coffee post

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,29 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 
-handleCoffeeRoute = (res) => {
+handleCoffeeRoute = (req,res) => {
   const data = [{
     "name": "Cà phê đá", "price": 10000, "currency": "VND"
   }, {
     "name": "Cà phê sữa", "price": 10000, "currency": "VND"
   }]
-  switch (res.method) {
+  switch (req.method) {
     case 'GET':
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json')
       res.end(JSON.stringify(data));
       break;
     case "POST":
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'application/json')
+      data.push({"name": "Bạc Xỉu", "price": 10000, "currency": "VND"})
+      res.end(JSON.stringify(data));
+      break;
       // TODO: add another coffee type and return the whole array
     default:
-      res.statusCode(405)
+      res.statusCode = 405
       res.end("Method not allowed")
+      break;
   }
 }
 
@@ -33,7 +39,7 @@ const server = http.createServer((req, res) => {
       res.end('Hello World');
       break;
     case "/coffee":
-      handleCoffeeRoute(res);
+      handleCoffeeRoute(req,res);
       break;
     default:
       res.statusCode = 404;
