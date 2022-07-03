0c883997e75270e10044852a531be560b98eec34
Loc Phan <pptloc28@Gmail.com>
2022-07-03 11:22:07 +0700

    app morgan

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@
 // old syntax
 // const express = require('express')
 import express from 'express'
+import morgan from 'morgan'
 // Create new express app
 const app = express()
 
+app.use(morgan('tiny'))
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('ready');
+  }
+)
