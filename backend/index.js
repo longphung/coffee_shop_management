06d84c21e59594fc84cd073e2bcdeb9a12479815
Loc Phan <pptloc28@Gmail.com>
2022-07-03 11:35:41 +0700

    create get to coffee route

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,16 @@
 // const express = require('express')
 import express from 'express'
 import morgan from 'morgan'
-import sampleCoffeeData from 'data.json'
+import sampleCoffeeData from './data.json' assert {type:"json"}
 // Create new express app
 const app = express()
 
 app.use(morgan('tiny'))
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+app.get('/coffee', (req,res) => {
+  res.json(sampleCoffeeData)
 })
 // TODO: Create GET to '/coffee', return sampleCoffeeData. Use express response.json() function
 
