8248119ffb7aa462fd57c28cab57d436bd7445be
Phung Nguyen <longphung.nn@gmail.com>
2022-07-03 11:24:24 +0700

    📚 docs: add todo for /coffee

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@
 // const express = require('express')
 import express from 'express'
 import morgan from 'morgan'
+import sampleCoffeeData from 'data.json'
 // Create new express app
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(morgan('tiny'))
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+// TODO: Create GET to '/coffee', return sampleCoffeeData. Use express response.json() function
 
 app.listen(3000, () => {
   console.log('ready');
